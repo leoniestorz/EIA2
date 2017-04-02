3db1e41c67bcffbdd802945040801e7a8b00419f
leoni <leoni@leonielaptop.speedport.ip>
2017-04-02 16:04:10 +0200

    Vollständige Aufgabe 2a hochgeladen

---
+++ Aufgabe 2/aufgabe2.js
@@ -0,0 +1,53 @@
+//Aufgabe: 2a - DynHTML: Sissa Ibn Dahir
+//Name: Leonie Storz
+//Matrikel: 255077
+//Datum: 02.04.17
+//    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    var n = 64;
+    var x = 1;
+    var c;
+    var f;
+    var zeile = 0;
+    var koerner = 1;
+    var div;
+    for (var i = 0; i < n; i++) {
+        if (i % 8 == 0) {
+            zeile++;
+        }
+        if (zeile % 2 == 0) {
+            if (i % 2 == 0) {
+                c = "#ffffff";
+                f = "#000000";
+            }
+            if (i % 2 == 1) {
+                c = "#000000";
+                f = "#ffffff";
+            }
+        }
+        if (zeile % 2 == 1) {
+            if (i % 2 == 1) {
+                c = "#ffffff";
+                f = "#000000";
+            }
+            if (i % 2 == 0) {
+                c = "#000000";
+                f = "#ffffff";
+            }
+        }
+        placeDiv(c, f);
+    }
+    function placeDiv(_color, _farbe) {
+        var div = document.createElement("div");
+        document.body.appendChild(div);
+        div.className += "felder";
+        div.innerText = "" + koerner;
+        koerner = koerner * 2;
+        var s = div.style;
+        s.backgroundColor = _color;
+        var f = div.style;
+        f.color = _farbe;
+    }
+});
+//# sourceMappingURL=aufgabe2.js.map
---
+++ Aufgabe 2/aufgabe2.ts
@@ -0,0 +1,78 @@
+ 
+//Aufgabe: 2a - DynHTML: Sissa Ibn Dahir
+//Name: Leonie Storz
+//Matrikel: 255077
+//Datum: 02.04.17
+//    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+
+document.addEventListener('DOMContentLoaded', function () {
+ 
+
+let n :number = 64;
+let x :number = 1;
+let c :string;
+let f:string;
+let zeile:number = 0;
+let koerner:number = 1;
+let div:any;
+
+    
+for (let i : number = 0; i < n ; i++){
+
+    
+    
+    if (i % 8 == 0){
+                  zeile++;}
+    
+   
+    if (zeile % 2 == 0){
+   
+            if (i % 2 == 0){
+                          c = "#ffffff";
+                          f = "#000000";}
+        
+            if (i % 2 == 1){
+                          c = "#000000";
+                          f="#ffffff";}
+    }
+    
+    if (zeile % 2 == 1){
+   
+            if (i % 2 == 1){
+                          c = "#ffffff";
+                          f = "#000000";}
+        
+            if (i % 2 == 0){
+                          c = "#000000";
+                          f = "#ffffff";}
+    }
+    
+            
+     placeDiv(c,f);
+ 
+         
+               
+    }
+
+    
+    function placeDiv(_color: string,_farbe:string): void {
+        
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        
+        div.className += "felder";
+        
+        div.innerText = "" + koerner;
+        koerner = koerner * 2;
+        
+
+        let s: CSSStyleDeclaration = div.style;
+        s.backgroundColor=_color;
+        
+        let f: CSSStyleDeclaration = div.style;
+        f.color=_farbe;
+        
+}
+});           
