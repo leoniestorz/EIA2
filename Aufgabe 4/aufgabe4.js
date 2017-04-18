38d348ce1437252399c6b59f68365029452b36e2
leoni <leoni@leonielaptop.speedport.ip>
2017-04-18 18:30:57 +0200

    Aufgabe 4 aktuelle Landschaft

--- Aufgabe 4/aufgabe4.js
+++ Aufgabe 4/aufgabe4.js
@@ -14,15 +14,17 @@ var aufgabe4_Canvas;
         drawWiese(0, 0, "#90EE90", "#90EE90");
         drawHimmel(0, 0, "##48d1CC", "#48d1CC");
         drawBerg1(500, 400, "#a9a9a9", "#a9a9a9");
-        drawBerg2(250, 400, "#696969", "#696969");
+        drawBerg2(250, 400);
         drawSonne(560, 120, "#FFFF00", "#FFFF00");
         drawBaum(750, 300);
-        drawBuschGross(560, 120, "#556B2F", "#556B2F");
-        drawBuschKlein(560, 120, "#556B2F", "#556B2F");
-        drawWolke(560, 120, "#FFFFFF", "#FFFFFF");
-        drawMohnblume(518, 565);
-        drawSonnenblume(560, 565);
-        drawTulpe(800, 600);
+        drawBuschGross(860, 380, "#556B2F", "#556B2F");
+        drawBuschKlein(90, 380, "#556B2F", "#556B2F");
+        drawBuschGross(-20, 440, "#556B2F", "#556B2F");
+        drawWolke(160, 120, "#FFFFFF", "#FFFFFF");
+        drawWolke(750, 180, "#FFFFFF", "#FFFFFF");
+        drawMohnblume(550, 565);
+        drawSonnenblume(760, 465);
+        drawTulpe(900, 600);
         function drawWiese(_x, _y, _strokeColor, _fillColor) {
             crc2.beginPath();
             crc2.fillStyle = _fillColor;
@@ -58,16 +60,23 @@ var aufgabe4_Canvas;
             crc2.fill();
             crc2.stroke();
         }
-        function drawBerg2(_x, _y, _strokeColor, _fillColor) {
+        function drawBerg2(_x, _y) {
+            //        crc2.beginPath();
+            //        crc2.strokeStyle = "#000000";
+            //        crc2.moveTo(451, 199);
+            //        crc2.lineTo(_x + 260, _y - 160);
+            //        crc2.lineTo(_x + 160, _y - 160);
+            //        crc2.lineTo(_x + 199, _y - 60);
+            //        crc2.stroke();
             crc2.beginPath();
-            crc2.fillStyle = _fillColor;
-            crc2.strokeStyle = _strokeColor;
+            crc2.fillStyle = "#696969";
+            crc2.strokeStyle = "#696969";
             crc2.moveTo(_x + 200, _y - 200);
             crc2.lineTo(_x + 400, _y);
             crc2.lineTo(_x, _y);
-            crc2.closePath();
             crc2.fill();
             crc2.stroke();
+            crc2.closePath();
         }
         function drawSonne(_x, _y, _strokeColor, _fillColor) {
             crc2.beginPath();
@@ -118,43 +127,49 @@ var aufgabe4_Canvas;
         function drawBuschGross(_x, _y, _strokeColor, _fillColor) {
             crc2.beginPath();
             crc2.fillStyle = _fillColor;
-            crc2.arc(910, 355, 25, 0, 2 * Math.PI);
-            crc2.arc(940, 375, 25, 0, 2 * Math.PI);
-            crc2.arc(910, 385, 35, 0, 2 * Math.PI);
-            crc2.arc(865, 390, 25, 0, 2 * Math.PI);
-            crc2.arc(865, 365, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 55, _y + 30, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 75, _y + 10, 20, 0, 2 * Math.PI);
+            crc2.arc(_x + 120, _y + 40, 24, 0, 2 * Math.PI);
+            crc2.arc(_x + 90, _y + 30, 35, 0, 2 * Math.PI);
             crc2.fill();
             crc2.closePath();
         }
         function drawBuschKlein(_x, _y, _strokeColor, _fillColor) {
             crc2.beginPath();
             crc2.fillStyle = _fillColor;
-            crc2.arc(170, 355, 25, 0, 2 * Math.PI);
-            crc2.arc(200, 375, 25, 0, 2 * Math.PI);
-            crc2.arc(170, 385, 35, 0, 2 * Math.PI);
-            crc2.arc(125, 390, 25, 0, 2 * Math.PI);
-            crc2.arc(125, 365, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 20, _y + 30, 28, 0, 2 * Math.PI);
+            crc2.arc(_x + 50, _y + 10, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 80, _y + 15, 20, 0, 2 * Math.PI);
+            crc2.arc(_x + 55, _y + 30, 25, 0, 2 * Math.PI);
             crc2.fill();
             crc2.closePath();
         }
         function drawWolke(_x, _y, _strokeColor, _fillColor) {
             crc2.beginPath();
             crc2.fillStyle = _fillColor;
-            crc2.arc(270, 155, 25, 0, 2 * Math.PI);
-            crc2.arc(300, 175, 25, 0, 2 * Math.PI);
-            crc2.arc(270, 185, 35, 0, 2 * Math.PI);
-            crc2.arc(225, 190, 25, 0, 2 * Math.PI);
-            crc2.arc(225, 165, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 10, _y + 30, 25, 0, 2 * Math.PI);
+            crc2.arc(_x + 50, _y + 25, 40, 0, 2 * Math.PI);
+            crc2.arc(_x + 90, _y + 20, 35, 0, 2 * Math.PI);
+            crc2.arc(_x + 130, _y + 20, 25, 0, 2 * Math.PI);
             crc2.fill();
             crc2.closePath();
         }
         function drawMohnblume(_x, _y) {
             crc2.beginPath();
             crc2.fillStyle = "#556B2F";
-            crc2.fillRect(508, 553, 4, 55);
+            crc2.strokeStyle = "#556B2F";
+            crc2.fillRect(_x - 10, _y + 5, 4, 55);
+            crc2.moveTo(_x - 10, _y + 30);
+            crc2.lineTo(_x + 5, _y + 15);
+            crc2.moveTo(_x - 10, _y + 40);
+            crc2.lineTo(_x - 20, _y + 30);
+            crc2.moveTo(_x - 10, _y + 55);
+            crc2.lineTo(_x + 2, _y + 40);
             crc2.fill();
+            crc2.stroke();
             crc2.beginPath();
             crc2.fillStyle = "#8B0000";
+            crc2.moveTo(_x, _y);
             crc2.arc(_x - 5, _y - 20, 10, 0, 2 * Math.PI);
             crc2.arc(_x + 4, _y - 8, 10, 0, 2 * Math.PI);
             crc2.arc(_x - 10, _y + 2, 10, 0, 2 * Math.PI);
@@ -162,15 +177,20 @@ var aufgabe4_Canvas;
             crc2.fill();
             crc2.beginPath();
             crc2.fillStyle = "#000000";
-            crc2.arc(510, 555, 5, 0, 2 * Math.PI);
+            crc2.arc(_x - 8, _y - 7, 5, 0, 2 * Math.PI);
             crc2.fill();
             crc2.closePath();
         }
         function drawSonnenblume(_x, _y) {
             crc2.beginPath();
             crc2.fillStyle = "#556B2F";
+            crc2.strokeStyle = "#556B2F";
             crc2.fillRect(_x + 68, _y + 30, 3, 55);
+            crc2.lineTo(_x + 70, _y + 50);
+            crc2.lineTo(_x + 60, _y + 50);
+            crc2.lineTo(_x + 70, _y + 70);
             crc2.fill();
+            crc2.stroke();
             crc2.beginPath();
             crc2.fillStyle = "#FFD700";
             crc2.moveTo(_x + 62, _y + 24);
