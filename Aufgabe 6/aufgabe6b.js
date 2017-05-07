1192231dba1b8b9903f6ca97d6a813544d873ac9
leoni <leoni@leonielaptop.fritz.box>
2017-05-07 15:31:47 +0200

    Aufgabe 6a und 6b hochgeladen

--- Aufgabe 6/aufgabe6b.js
+++ Aufgabe 6/aufgabe6b.js
@@ -31,24 +31,58 @@ var StudiVZ;
         var InputArray = _input.split(",");
         var studi = {
             Matrikelnummer: parseInt(InputArray[0]),
-            Vorname: InputArray[1],
-            Name: InputArray[2],
+            Vorname: (InputArray[1]),
+            Name: (InputArray[2]),
             Alter: parseInt(InputArray[3]),
             Geschlecht: parseInt(InputArray[4]) == 0,
-            Kommentar: InputArray[5],
-        };
-        students.push(studi);
-        var Geschlecht;
-        if (studi.Geschlecht == true) {
-            Geschlecht = "m";
+            Kommentar: (InputArray[5]), };
+        //Angabe des Geschlechts
+        var geschlechtEingabe;
+        if (parseInt(InputArray[4]) == 0) {
+            geschlechtEingabe = "maennlich";
         }
         else {
-            Geschlecht = "w";
+            geschlechtEingabe = "weiblich";
         }
-        return "Ihre Daten wurden gespeichert: \nMatrikel: " + +"\nVorname:" + "\nName:" + +"\nAlter:" + +"\nGeschlecht" + +"\nKommentar";
+        //Ungueltige Eingabe, wenn mind. eine Angabe fehlt      
+        if (InputArray.length <= 5) {
+            return "Ihre Eingabe enthielt nicht alle benoetigten Angaben.\nBitte erneut versuchen";
+        }
+        //Ungueltige Matrikelnummer, wenn diese nicht in Zahlen eingegeben wurde        
+        if (isNaN(parseInt(InputArray[0]))) {
+            return "Ihre Eingabe ist ungueltig.\nBitte geben Sie Ihre Martikelnummer als Zahl ein.";
+        }
+        //Ungueltige Eingabe f�r Geschlecht, wenn nicht die Zahlen 0 oder 1 eingegeben wurden     
+        if (parseInt(InputArray[4]) != 0 && parseInt(InputArray[4]) != 1) {
+            return "Ihre Eingabe ist ungeltig.\nBitte geben Sie f�r das Geschlecht 0 (m�nnlich) oder 1 (weiblich) ein.";
+        }
+        //Ungueltige Alterseingabe, wenn dieses nicht in Zahlen eingegeben wurde
+        if (isNaN(parseInt(InputArray[3]))) {
+            return "Ihre Eingabe ist ungueltig.\nBitte geben Sie das Alter als Zahl an.";
+        }
+        //Eingaben in Array students pushen
+        students.push(studi);
+        //Textausgabe bei erfolgreicher Speicherung der Daten
+        return "Ihre Daten wurden gespeichert: \n\nMatrikel:" + studi.Matrikelnummer + "\nVorname:" + studi.Vorname + "\nName:" + studi.Name + "\nAlter:" + studi.Alter + "\nGeschlecht:" + geschlechtEingabe + "\nKommentar:" + studi.Kommentar;
     }
+    //Funktion zur Ausgabe der gespeicherten Daten
     function queryData(_matrikel) {
-        return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \nMatrikelnummer:" + +"\nVorname:" + +"\nAlter:" + +"\nGeschlecht" + +"\nKommentar";
+        for (var i = 0; i < students.length; i++) {
+            var geschlechtAusgabe = void 0;
+            if (students[i].Geschlecht == false) {
+                geschlechtAusgabe = "maennlich";
+            }
+            else {
+                geschlechtAusgabe = "weiblich";
+            }
+            if (students[i].Matrikelnummer == _matrikel) {
+                //Textausgabe bei erfolgreichen Findung der gepeicherten Daten   
+                return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \n\nMatrikel: " + students[i].Matrikelnummer + "\nVorname:" + students[i].Vorname + "\nName:" + students[i].Name + "\nAlter:" + students[i].Alter + "\nGeschlecht:" + geschlechtAusgabe + "\nKommentar:" + students[i].Kommentar;
+            }
+            else {
+                return "Fuer diese Matrikelnummer ist noch kein Eintrag vorhanden.";
+            }
+        }
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=aufgabe6b.js.map
--- Aufgabe 6/aufgabe6b.ts
+++ Aufgabe 6/aufgabe6b.ts
@@ -49,31 +49,79 @@ namespace StudiVZ {
         let studi : StudentData = {
             
             Matrikelnummer : parseInt(InputArray[0]),
-            Vorname : InputArray[1],
-            Name : InputArray [2],
+            Vorname : (InputArray[1]),
+            Name : (InputArray [2]),
             Alter : parseInt(InputArray[3]), 
             Geschlecht : parseInt(InputArray[4]) == 0,
-            Kommentar : InputArray [5],
-            
-            }
+            Kommentar : (InputArray [5]),}
         
-         students.push(studi)
+       
+//Angabe des Geschlechts
+                
+       let geschlechtEingabe: string;
         
-        let Geschlecht: string;
-      
-        if (studi.Geschlecht == true) {
-           Geschlecht = "m";}
-        else {
-           Geschlecht = "w";}
+       if (parseInt(InputArray[4]) == 0) 
+       
+            {geschlechtEingabe = "maennlich";}
+       
+       else {geschlechtEingabe = "weiblich";} 
+
         
-         return "Ihre Daten wurden gespeichert: \nMatrikel: " +    + "\nVorname:" +   "\nName:" +   + "\nAlter:" +   + "\nGeschlecht"+   +"\nKommentar"
+//Ungueltige Eingabe, wenn mind. eine Angabe fehlt      
+        
+        if(InputArray.length <= 5) {
+        return "Ihre Eingabe enthielt nicht alle benoetigten Angaben.\nBitte erneut versuchen";}
+
+//Ungueltige Matrikelnummer, wenn diese nicht in Zahlen eingegeben wurde        
+        
+        if(isNaN(parseInt(InputArray[0]))){
+        return "Ihre Eingabe ist ungueltig.\nBitte geben Sie Ihre Martikelnummer als Zahl ein.";}
+
+//Ungueltige Eingabe für Geschlecht, wenn nicht die Zahlen 0 oder 1 eingegeben wurden     
+        if (parseInt(InputArray[4]) != 0 && parseInt(InputArray[4]) != 1){
+        return "Ihre Eingabe ist ungeltig.\nBitte geben Sie für das Geschlecht 0 (männlich) oder 1 (weiblich) ein.";}
+
+//Ungueltige Alterseingabe, wenn dieses nicht in Zahlen eingegeben wurde
+        
+        if(isNaN(parseInt(InputArray[3]))){
+        return "Ihre Eingabe ist ungueltig.\nBitte geben Sie das Alter als Zahl an.";}
+
+//Eingaben in Array students pushen
+        
+        students.push(studi);
+        
+//Textausgabe bei erfolgreicher Speicherung der Daten
+               
+         return "Ihre Daten wurden gespeichert: \n\nMatrikel:"+ studi.Matrikelnummer + "\nVorname:" + studi.Vorname + "\nName:"  + studi.Name + "\nAlter:"  + studi.Alter + "\nGeschlecht:" + geschlechtEingabe +"\nKommentar:" +studi.Kommentar;
      
     }
+
+//Funktion zur Ausgabe der gespeicherten Daten
     
     function queryData(_matrikel: number): string {
         
+        for (let i: number = 0; i < students.length; i++) {
         
-        
-        return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \nMatrikelnummer:" +   + "\nVorname:" +     + "\nAlter:"+     +"\nGeschlecht"+   + "\nKommentar" 
-    }
+              
+
+            let geschlechtAusgabe: string;
+                
+            if (students[i].Geschlecht == false)
+                  
+                  {geschlechtAusgabe = "maennlich";}
+              
+             else {geschlechtAusgabe = "weiblich";}
+            
+            
+         if (students[i].Matrikelnummer == _matrikel){    
+                
+//Textausgabe bei erfolgreichen Findung der gepeicherten Daten   
+                     
+        return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \n\nMatrikel: " + students[i].Matrikelnummer + "\nVorname:" + students[i].Vorname + "\nName:" + students[i].Name + "\nAlter:" + students[i].Alter + "\nGeschlecht:"+ geschlechtAusgabe +"\nKommentar:" + students[i].Kommentar;}
+
+//Textausgabe, falls unter eingegebener Matrikelnummer keine Daten gefunden wurden   
+                
+        else {return "Fuer diese Matrikelnummer ist noch kein Eintrag vorhanden."}
 }
+   }
+    }
