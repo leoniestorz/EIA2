b07c776924f8603aaad56d511fc977ac60eeb3dd
leoni <leoni@leonielaptop.local>
2017-05-10 14:16:20 +0200

    Beginn Aufgabe 7

---
+++ Aufgabe 7/blumen7.js
@@ -0,0 +1,8 @@
+//Aufgabe: 7 - OO-Blumenwiese
+//Name: Leonie Storz
+//Matrikel: 255077
+//Datum: 14.05.17
+//    
+//Hiermit versichere ich, dass ich diesen Code selbst erstellt habe. 
+//Er wurde nicht kopiert und auch nicht diktiert.  
+//# sourceMappingURL=blumen7.js.map
